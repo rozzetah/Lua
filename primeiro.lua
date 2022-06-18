cee2bfdc86db164bb186c8fcb9b35f645589323e
rozzetah <marcioairesdarosa@gmail.com>
2022-06-18 16:16:13 -0300

    Arquivos de aprendizado em Lua.

---
+++ primeiro.lua
@@ -0,0 +1,24 @@
+-- Tipos e variaveis em Lua.
+
+local energia = 13
+local nome = "Márcio"
+local passoufase = false
+
+-- Somando valores.
+
+local maisenergia = 4
+print (energia + maisenergia)
+
+-- Concatenando valores.
+
+print ('O nome do jogodor é: ' .. nome)
+
+-- Verificando tipos.
+print (type(energia))
+print (type(nome))
+print (type(passoufase))
+
+-- Operadores relacionais.
+print (energia == maisenergia)
+print (energia == maisenergia)
+print (energia > maisenergia)
